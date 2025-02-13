1dea170a71aa43d3c2a5ece8dd1be5fa6c2ea35c
LeoGHPH <gamesspace33@gmail.com>
2025-02-13 19:15:15 -0300

    Update planeta.dart

--- lib/modelos/planeta.dart
+++ lib/modelos/planeta.dart
@@ -13,31 +13,29 @@ class Planeta {
     this.apelido,
   });
   Planeta.vazio()
-      : id = null,
-        nome = '',
-        tamanho = 0.0,
-        distancia = 0.0,
-        apelido = '';
+    : id = null,
+      nome = '',
+      tamanho = 0.0,
+      distancia = 0.0,
+      apelido = '';
 
   factory Planeta.fromMap(Map<String, dynamic> map) {
-  return Planeta(
-    id: map['id'],
-    nome: map['nome'],
-    tamanho: map['tamanho'],
-    distancia: map['distancia'],
-    apelido: map['apelido'],
-  );
-}
+    return Planeta(
+      id: map['id'],
+      nome: map['nome'],
+      tamanho: map['tamanho'],
+      distancia: map['distancia'],
+      apelido: map['apelido'],
+    );
+  }
 
-
-Map<String, dynamic> toMap() {
-  return {
-    'id': id,
-    'nome': nome,
-    'tamanho': tamanho,
-    'distancia': distancia,
-    'apelido': apelido,
-  };
-}
-
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'tamanho': tamanho,
+      'distancia': distancia,
+      'apelido': apelido,
+    };
+  }
 }
